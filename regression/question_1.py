da7f35f9136bff2cf3562befef746b9f6be25342
Trien Tran <aunhi55@gmail.com>
2023-03-25 23:31:56 +1030

    Reformat question 1

--- regression/question_1.py
+++ regression/question_1.py
@@ -32,26 +32,28 @@ def regression_model():
     return model
 
 
-# Run and save your model
-my_model = regression_model()
-model_name = "regression_model_1.h5"
-my_model.save(model_name)
+# ===============DO NOT EDIT THIS PART================================
+if __name__ == '__main__':  # Run and save your model
+    # Run and save your model
+    my_model = regression_model()
+    model_name = "regression_model_1.h5"
+    my_model.save(model_name)
 
-# Reload the saved model
-saved_model = tf.keras.models.load_model(model_name)
+    # Reload the saved model
+    saved_model = tf.keras.models.load_model(model_name)
 
-# Show the model architecture
-saved_model.summary()
+    # Show the model architecture
+    saved_model.summary()
 
-# Test the model on some new data
-x_test = np.array([7, 8, 9, 10])
-y_test = np.array([75, 85, 95, 105])
-predictions = saved_model.predict(x_test)
+    # Test the model on some new data
+    x_test = np.array([7, 8, 9, 10])
+    y_test = np.array([75, 85, 95, 105])
+    predictions = saved_model.predict(x_test)
 
-# Print the predictions and expected values
-for i in range(len(x_test)):
-    print("x = {:.0f}, expected y = {:.0f}, predicted y = {:.0f}".format(x_test[i], y_test[i], predictions[i][0]))
+    # Print the predictions and expected values
+    for i in range(len(x_test)):
+        print("x = {:.0f}, expected y = {:.0f}, predicted y = {:.0f}".format(x_test[i], y_test[i], predictions[i][0]))
 
-# Evaluate the model on the test data
-test_loss = saved_model.evaluate(x_test, y_test)
-print("Test loss: {:.2f}".format(test_loss))
+    # Evaluate the model on the test data
+    test_loss = saved_model.evaluate(x_test, y_test)
+    print("Test loss: {:.2f}".format(test_loss))
